324e10744fb96c76907d58076c7be651889397a6
Benjamin McLean <mclean.ben@gmail.com>
2019-08-10 10:09:18 -0500

    Added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+The com.albert.wolf3d.core.io.file package contains code by [kalbert312](http://github.com/kalbert312).
---
+++ src/net/benmclean/wolf3dviewer/Main.java
@@ -0,0 +1,18 @@
+package net.benmclean.wolf3dviewer;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    public Main() {
+        setTitle("WOLF3D-Viewer");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Main ex = new Main();
+        ex.setVisible(true);
+    }
+}
